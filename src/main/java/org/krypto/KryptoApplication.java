d5323e5f91f942e6e6ca2a85430423d56c39b9e5
Tomunek <kowalczyk.tomek11@gmail.com>
2023-03-18 21:06:30 +0100

    Added some testing code

--- src/main/java/org/krypto/KryptoApplication.java
+++ src/main/java/org/krypto/KryptoApplication.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HexFormat;
 import java.util.Objects;
 
 // Key-yellow.png from https://freesvg.org/yellow-lock-key-vector-image (public domain)
@@ -16,6 +17,18 @@ public class KryptoApplication extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            // TESTING
+//            byte[] key = {0};
+//            AES aes = new AES(key);
+//            byte[] plaintext = {0,0,0,0};
+//            byte[] ciphertext = {0};
+//            ciphertext = aes.encrypyData(plaintext);
+//            HexFormat hex = HexFormat.of();
+//            System.out.println(hex.formatHex(plaintext));
+//            System.out.println(plaintext.length);
+//            System.out.println(hex.formatHex(ciphertext));
+//            System.out.println(ciphertext.length);
+            // TESTING
             Parent root = FXMLLoader.load(Objects.requireNonNull(KryptoApplication.class.getResource("/org.krypto/aes.fxml")));
             Scene scene = new Scene(root);
             stage.setScene(scene);
