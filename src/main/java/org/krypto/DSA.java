f57d6e8fd3f843647d68bc692a5891a28a71937c
Tomunek <kowalczyk.tomek11@gmail.com>
2023-04-07 13:03:52 +0200

    Implemented most math for DSA

--- src/main/java/org/krypto/DSA.java
+++ src/main/java/org/krypto/DSA.java
@@ -1,18 +1,80 @@
 package org.krypto;
 
-public class DSA implements Cipher{
-    @Override
-    public void setKey(byte[] key) {
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
+public class DSA implements Sign {
+    private BigInteger p;
+    private BigInteger q;
+    private BigInteger h;
+    private BigInteger privateKey;
+    private BigInteger publicKey;
+
+    @Override
+    public void setPublicKey(BigInteger key) {
+        publicKey = key;
     }
 
     @Override
-    public byte[] encryptData(byte[] plaintext) {
-        return new byte[0];
+    public void setPrivateKey(BigInteger key) {
+        privateKey = key;
     }
 
     @Override
-    public byte[] decryptData(byte[] ciphertext) {
-        return new byte[0];
+    public void setParams(BigInteger p, BigInteger q, BigInteger h) {
+        this.p = p;
+        this.q = q;
+        this.h = h;
+    }
+
+    private BigInteger getDocumentHashAsBigInt(byte[] document) {
+        BigInteger document_hash;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] hash = md.digest(document);
+            document_hash = new BigInteger(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return document_hash;
+    }
+
+    @Override
+    public Signature signData(byte[] data) {
+        // TODO: add error checking for empty private key / params
+        // TODO: generate random r (0 < r <= q-1)
+        // 1) generate random r (0 < r <= q-1)
+        BigInteger r = new BigInteger("0");
+        // 2) calculate r' = r^-1 mod q
+        BigInteger r_prime = r.modInverse(q);
+        Signature s = new Signature();
+        // 3) calculate s1 = (h^r mod p) mod q
+        s.s1 = (h.modPow(r, p)).mod(q);
+        // calculate documents hash
+        BigInteger document_hash = getDocumentHashAsBigInt(data);
+        // 4) calculate s2 = (r'(SHA512(doc) + as1)) mod q
+        //      (    r'    *     (      SHA512    +           a   *    s1       )) mod q
+        s.s2 = (r_prime.multiply((document_hash.add(privateKey.multiply(s.s1))))).mod(q);
+        return s;
+    }
+
+    @Override
+    public boolean verifySignature(byte[] data, Signature s) {
+        // TODO: add error checking for empty public key / params
+        // 1) calculate s' = s2 ^-1 mod q
+        BigInteger s_prime = s.s2.modInverse(q);
+        // calculate documents hash
+        BigInteger document_hash = getDocumentHashAsBigInt(data);
+        // 2) calculate u1 = (SHA512(doc) s') mod q
+        BigInteger u1 = (document_hash.multiply(s_prime)).mod(q);
+        // 3) calculate u2 = (s' s1) mod q
+        BigInteger u2 = (s_prime.multiply(s.s1)).mod(q);
+        // 4) calculate t = (h^u1 b^u2 mod p) mod q
+        // TODO: this might not be ok (check)
+        BigInteger t = ((h.modPow(u1, p).multiply(publicKey.modPow(u2, p))).mod(p)).mod(q);
+        // 5) check signature
+        return t.equals(s.s1);
     }
 }
